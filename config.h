81709afeb8e2ca6c9dede361e489155822f36c08
David Zhang <kiteabpl@outlook.com>
2020-07-08 18:40:39 +0800

    tile 2 t

--- config.h
+++ config.h
@@ -63,7 +63,7 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "Tile",      tile },    /* first entry is default */
+	{ "T",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
