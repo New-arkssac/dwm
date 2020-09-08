7da996bbb98552bc077c29aec855ec3c699e036e
New-arkssac <mrarkssac@gmail.com>
2020-09-08 16:07:06 +0800

    my-new-dwm

--- config.h
+++ config.h
@@ -3,22 +3,22 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 5;
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const unsigned int systrayspacing = 1;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=15" };
-static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=15";
+static const char *fonts[]          = { "SourceCodePro-Black:size=15" };
+static const char dmenufont[]       = "SourceCodePro-Black:size=15";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -63,13 +63,13 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "Tile",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "XD",      tile },    /* first entry is default */
+	{ "=_=",      NULL },    /* no layout function means floating behavior */
+	{ "[-]",      monocle },
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask
+#define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -83,34 +83,38 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[]  = { "google-chrome-stable", NULL };
+static const char *browsercmd[]  = { "firefox", NULL };
 
-static const char *upvol[]   = { "/home/kiteab/scripts/vol-up.sh",  NULL };
-static const char *downvol[] = { "/home/kiteab/scripts/vol-down.sh",  NULL };
-static const char *mutevol[] = { "/home/kiteab/scripts/vol-toggle.sh",  NULL };
+static const char *upvol[]   = { "/home/arkssac/scripts/vol-up.sh",  NULL };
+static const char *downvol[] = { "/home/arkssac/scripts/vol-down.sh",  NULL };
+static const char *mutevol[] = { "/home/arkssac/scripts/vol-toggle.sh",  NULL };
 
-static const char *wpcmd[]  = { "/home/kiteab/scripts/wp-change.sh", NULL };
-static const char *sktogglecmd[]  = { "/home/kiteab/scripts/sck-tog.sh", NULL };
-static const char *nautilustogcmd[]  = { "/home/kiteab/scripts/nautilus-tog.sh", NULL };
+static const char *wpcmd[]  = { "/home/arkssac/scripts/wp-change.sh", NULL };
+static const char *sktogglecmd[]  = { "/home/arkssac/scripts/sck-tog.sh", NULL };
+static const char *nautilustogcmd[]  = { "/home/arkssac/scripts/nautilus-tog.sh", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "80x24", NULL };
 
-static const char *setqwertycmd[]  = { "/home/kiteab/scripts/setxmodmap-qwerty.sh", NULL };
-static const char *setcolemakcmd[]  = { "/home/kiteab/scripts/setxmodmap-colemak.sh", NULL };
+static const char *setqwertycmd[]  = { "/home/arkssac/scripts/setxmodmap-qwerty.sh", NULL };
+static const char *setcolemakcmd[]  = { "/home/arkssac/scripts/setxmodmap-colemak.sh", NULL };
 
-static const char *suspendcmd[]  = { "/home/kiteab/scripts/suspend.sh", NULL };
+static const char *suspendcmd[]  = { "/home/arkssac/scripts/suspend.sh", NULL };
 
-static const char *screenlockcmd[]  = { "/home/kiteab/scripts/screenlock.sh", NULL};
+static const char *screenlockcmd[]  = { "/home/arkssac/scripts/screenlock.sh", NULL};
 
-static const char *incbacklightcmd[]  = { "/home/kiteab/scripts/inc-backlight.sh", NULL};
-static const char *decbacklightcmd[]  = { "/home/kiteab/scripts/dec-backlight.sh", NULL};
+static const char *incbacklightcmd[]  = { "/home/arkssac/scripts/inc-backlight.sh", NULL};
+static const char *decbacklightcmd[]  = { "/home/arkssac/scripts/dec-backlight.sh", NULL};
 
 static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
+static const char *roifrun[] = { "rofi", "-show", "run", NULL };
+static const char *roriapp[] = { "rofi", "-show", "drun", NULL };
 
 static Key keys[] = {
 	/* modifier            key                      function        argument */
 	{ MODKEY,              XK_s,                    spawn,          {.v = dmenucmd } },
 	{ MODKEY,              XK_Return,               spawn,          {.v = termcmd } },
+	{ MODKEY,              XK_q,                    spawn,          {.v = roifrun } },
+	{ MODKEY,              XK_e,                    spawn,          {.v = roriapp } },
 	{ MODKEY,              XK_c,                    spawn,          {.v = browsercmd } },
 	{ MODKEY|ShiftMask,    XK_w,                    spawn,          {.v = setqwertycmd } },
 	{ MODKEY|ShiftMask,    XK_m,                    spawn,          {.v = setcolemakcmd } },
@@ -118,32 +122,32 @@ static Key keys[] = {
 	{ MODKEY|ControlMask,  XK_s,                    spawn,          {.v = sktogglecmd } },
 	{ MODKEY,              XK_f,                    spawn,          {.v = nautilustogcmd } },
 	{ MODKEY|ControlMask,  XK_l,                    spawn,          {.v = screenlockcmd } },
-	{ 0,                   XF86XK_AudioLowerVolume, spawn,          {.v = downvol } },
+	{ MODKEY|ShiftMask,    XK_F2,                   spawn,          {.v = downvol } },
 	{ 0,                   XF86XK_AudioMute,        spawn,          {.v = mutevol } },
-	{ 0,                   XF86XK_AudioRaiseVolume, spawn,          {.v = upvol   } },
-	{ MODKEY,              XK_bracketleft,          spawn,          {.v = downvol } },
+	{ MODKEY|ShiftMask,    XK_F3,                   spawn,          {.v = upvol   } },
 	{ MODKEY,              XK_backslash,            spawn,          {.v = mutevol } },
-	{ MODKEY,              XK_bracketright,         spawn,          {.v = upvol   } },
 	{ MODKEY,              XK_b,                    spawn,          {.v = wpcmd } },
 	{ MODKEY,              XK_comma,                spawn,          {.v = decbacklightcmd } },
 	{ MODKEY,              XK_period,               spawn,          {.v = incbacklightcmd } },
 	{ 0,                   XK_Print,                spawn,          {.v = screenshotcmd } },
-	{ MODKEY|ShiftMask,    XK_e,                    rotatestack,    {.i = +1 } },
-	{ MODKEY|ShiftMask,    XK_u,                    rotatestack,    {.i = -1 } },
-	{ MODKEY,              XK_e,                    focusstack,     {.i = +1 } },
-	{ MODKEY,              XK_u,                    focusstack,     {.i = -1 } },
-	{ MODKEY,              XK_n,                    viewtoleft,     {0} },
-	{ MODKEY,              XK_i,                    viewtoright,    {0} },
-	{ MODKEY|ShiftMask,    XK_n,                    tagtoleft,      {0} },
-	{ MODKEY|ShiftMask,    XK_i,                    tagtoright,     {0} },
+	{ MODKEY|ShiftMask,    XK_j,                    rotatestack,    {.i = +1 } },
+	{ MODKEY|ShiftMask,    XK_l,                    rotatestack,    {.i = -1 } },
+	{ MODKEY,              XK_Up,                   focusstack,     {.i = +1 } },
+	{ MODKEY,              XK_Left,                 focusstack,     {.i = +1 } },
+	{ MODKEY,              XK_Right,                focusstack,     {.i = -1 } },
+	{ MODKEY,              XK_Down,                 focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,    XK_u,                    viewtoleft,     {0} },
+	{ MODKEY|ShiftMask,    XK_o,                    viewtoright,    {0} },
+	{ MODKEY|ShiftMask,    XK_e,                    tagtoleft,      {0} },
+	{ MODKEY|ShiftMask,    XK_y,                    tagtoright,     {0} },
 	{ MODKEY|ShiftMask,    XK_h,                    incnmaster,     {.i = +1 } },
 	{ MODKEY|ShiftMask,    XK_l,                    incnmaster,     {.i = -1 } },
-	{ MODKEY,              XK_h,                    setmfact,       {.f = -0.05} },
-	{ MODKEY,              XK_l,                    setmfact,       {.f = +0.05} },
-	{ MODKEY,              XK_k,                    hidewin,        {0} },
-	{ MODKEY|ShiftMask,    XK_k,                    restorewin,     {0} },
-	{ MODKEY,              XK_o,                    hideotherwins,  {0}},
-	{ MODKEY|ShiftMask,    XK_o,                    restoreotherwins, {0}},
+	{ MODKEY|ShiftMask,              XK_F4,                   setmfact,       {.f = -0.05} },
+	{ MODKEY|ShiftMask,              XK_F1,                   setmfact,       {.f = +0.05} },
+	{ MODKEY,              XK_n,                    hidewin,        {0} },
+	{ MODKEY|ShiftMask,    XK_n,                    restorewin,     {0} },
+	{ MODKEY,              XK_r,                    hideotherwins,  {0}},
+	{ MODKEY|ShiftMask,    XK_r,                    restoreotherwins, {0}},
 	{ MODKEY|ShiftMask,    XK_b,                    togglebar,      {0} },
 	{ MODKEY|ShiftMask,    XK_Return,               zoom,           {0} },
 	{ MODKEY,              XK_Tab,                  view,           {0} },
@@ -151,9 +155,9 @@ static Key keys[] = {
 	{ MODKEY,              XK_t,                    setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,              XK_m,                    setlayout,      {.v = &layouts[2]} },
 	{ MODKEY|ControlMask,  XK_f,                    fullscreen,     {0} },
-	{ MODKEY|ShiftMask,    XK_f,                    togglefullscr,  {0} },
-	{ MODKEY,              XK_space,                setlayout,      {0} },
-	{ MODKEY|ShiftMask,    XK_space,                togglefloating, {0} },
+	{ MODKEY,              XK_space,                togglefullscr,  {0} },
+	{ MODKEY|ShiftMask,    XK_space,                setlayout,      {0} },
+	{ MODKEY|ShiftMask,    XK_minus,                togglefloating, {0} },
 	{ MODKEY,              XK_apostrophe,           togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,              XK_0,                    view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,    XK_0,                    tag,            {.ui = ~0 } },
